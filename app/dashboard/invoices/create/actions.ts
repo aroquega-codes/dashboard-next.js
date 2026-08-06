301842b2cf69b81e3c96522053b2d42fa7180f56
Hristo Roque <aroquega@gmail.com>
2026-08-06 10:11:58 -0500

    Se agrega funcionalidad de crear

---
+++ app/dashboard/invoices/create/actions.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
+import {z} from 'zod';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormDataSchema = z.object({
+    id: z.number(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.string(),
+    date: z.string()
+})
+
+const CreateInvoice = FormDataSchema.omit({id: true, date: true})
+
+export async function createInvoice(formData: FormData){
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    }
+    
+    const {customerId, amount, status} = CreateInvoice.parse(rawFormData);
+    const amountInCents = amount * 100;
+    
+    const date = new Date().toISOString().split('T')[0]
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
