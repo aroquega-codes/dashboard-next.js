615a878635f5535af28102a920e777fd461a4f7c
Hristo Roque <aroquega@gmail.com>
2026-08-07 10:11:23 -0500

    Se agrega error y pagina de no encontrado

--- app/dashboard/invoices/actions.ts
+++ app/dashboard/invoices/actions.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
-import {z} from 'zod';
+import { z } from 'zod';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -15,48 +15,71 @@ const FormDataSchema = z.object({
     date: z.string()
 })
 
-const CreateInvoice = FormDataSchema.omit({id: true, date: true})
+const CreateInvoice = FormDataSchema.omit({ id: true, date: true })
 
-export async function createInvoice(formData: FormData){
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    }
-    
-    const {customerId, amount, status} = CreateInvoice.parse(rawFormData);
-    const amountInCents = amount * 100;
-    
-    const date = new Date().toISOString().split('T')[0]
+export async function createInvoice(formData: FormData) {
+    try {
+        const rawFormData = {
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status'),
+        }
 
-    await sql`
+        const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+        const amountInCents = amount * 100;
+
+        const date = new Date().toISOString().split('T')[0]
+
+        await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  
-  revalidatePath('/dashboard/invoices')
-  redirect('/dashboard/invoices')
+    } catch (e) {
+        console.error(e);
+    }
+
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
 }
 
 const UpdateInvoice = FormDataSchema.omit({ id: true, date: true });
- 
+
 // ...
- 
+
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
- 
-  const amountInCents = amount * 100;
- 
-  await sql`
+    try {
+        const { customerId, amount, status } = UpdateInvoice.parse({
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status'),
+        });
+
+        const amountInCents = amount * 100;
+
+        await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    try {
+        await sql`
+    DELETE FROM invoices
+    WHERE id = ${id}
+  `;
+    } catch (e) {
+        redirect('/dashboard/invoices');
+    }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
